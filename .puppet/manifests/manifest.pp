d0a1626084204c786ac2c7f6a04afce515741108
Owen Barton <drupal@owenbarton.com>
2012-12-17 01:25:22 -0800

    Adding various bits of wpt server and driver configuration

--- .puppet/manifests/manifest.pp
+++ .puppet/manifests/manifest.pp
@@ -24,10 +24,110 @@ apache::vhost { 'localhost':
   docroot  => '/home/drupal/docroot',
 }
 percona::database { 'drupal':
-  ensure => present;
+  ensure => present,
 }
 percona::rights {'drupal@localhost/drupal':
   priv => 'all',
   password => 'drupal',
 }
+# WPT server specific
+exec { 'apache_instance_wpt':
+  command => '/usr/bin/sudo /bin/sh /usr/share/doc/apache2.2-common/examples/setup-instance wpt',
+  onlyif => 'test ! -d /etc/apache2-wpt',
+}
+class apache_wpt {
+  $service = 'apache2-wpt'
+  $config_dir = '/etc/apache2-wpt'
+  $config_file = '/etc/apache2-wpt/apache2.conf'
+  $config_file_mode = '0644'
+  $config_file_owner = 'root'
+  $config_file_group = 'root'
+  $vhost_dir = '/etc/apache2-wpt/sites-enabled'
+  $pid_file = '/var/run/apache2-wpt.pid'
+  $log_dir = '/var/log/apache2-wpt'
+  $log_file = ['/var/log/apache2-wpt/access.log','/var/log/apache2-wpt/error.log']
+  $port = '8888'
+  include apache
+}
+class {'apache_wpt': }
+file { "wpt_ports.conf":
+  ensure  => 'present',
+  path    => "${apache_wpt::config_dir}/ports.conf",
+  mode    => $apache_wpt::config_file_mode,
+  owner   => $apache_wpt::config_file_owner,
+  group   => $apache_wpt::config_file_group,
+  require => Package['apache'],
+  notify  => $apache_wpt::manage_service_autorestart,
+  content => template('wpt_ports.conf'),
+  audit   => $apache_wpt::manage_audit,
+}
+define apache_wpt::vhost (
+  $docroot        = '/home/drupal/wpt/www',
+  $docroot_create = false,
+  $docroot_owner  = 'root',
+  $docroot_group  = 'root',
+  $port           = '8888',
+  $ssl            = false,
+  $template       = 'apache/virtualhost/vhost.conf.erb',
+  $priority       = '50',
+  $serveraliases  = '',
+  $enable         = true ) {
 
+  $ensure = bool2ensure($enable)
+  $bool_docroot_create = any2bool($docroot_create)
+
+  $real_docroot = $docroot ? {
+    ''      => "${apache_wpt::data_dir}/${name}",
+    default => $docroot,
+  }
+
+  include apache_wpt
+
+  file { "${apache_wpt::vdir}/${priority}-${name}.conf":
+    ensure  => $ensure,
+    content => template($template),
+    mode    => $apache_wpt::config_file_mode,
+    owner   => $apache_wpt::config_file_owner,
+    group   => $apache_wpt::config_file_group,
+    require => Package['apache'],
+    notify  => $apache_wpt::manage_service_autorestart,
+  }
+
+  file { "ApacheVHostEnabled_$name":
+    ensure  => $enable ? {
+      true  => "${apache_wpt::vdir}/${priority}-${name}.conf",
+      false => absent,
+    },
+    path    => "${apache_wpt::config_dir}/sites-enabled/${priority}-${name}.conf",
+    require => Package['apache'],
+  }
+
+  if $bool_docroot_create == true {
+    file { $real_docroot:
+      ensure  => directory,
+      owner   => $docroot_owner,
+      group   => $docroot_group,
+      mode    => '0775',
+      require => Package['apache'],
+    }
+  }
+}
+apache_wpt::vhost { 'wpt': }
+# Test agent specific
+apt::source { 'chrome':
+  location => 'http://dl.google.com/linux/chrome/deb/',
+  repos => 'main',
+  release => 'stable',
+  include_src => false,
+  key_source => 'http://www.google.com/linuxrepositories/',
+}
+package { 'google-chrome-stable':
+  ensure => present,
+}
+apt::ppa { 'ppa:grugnog/ipfw': }
+package { 'ipfw3-utils':
+  ensure => present,
+}
+package { ['node', 'default-jdk']:
+  ensure => present,
+}
